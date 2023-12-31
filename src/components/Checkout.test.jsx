65f679729d00667d8a50895a9e8cc16b7f4796ed
mekbibDev <mekbibtare@gmail.com>
2023-12-31 06:51:31 +0300

    Write test for one and many values

--- src/components/Checkout.test.jsx
+++ src/components/Checkout.test.jsx
@@ -8,7 +8,7 @@ it("renders a helpful message when given an empty array of selected products", (
   ).not.toThrow();
 });
 
-it("renders the selected products when given an an array with a selected product", () =>{
+it("renders the selected product when given an an array with a selected product", () => {
   const product = {
     id: 0,
     title: "bluh",
@@ -19,5 +19,37 @@ it("renders the selected products when given an an array with a selected product
     selectedAmount: 1,
   };
   render(<Checkout selectedProducts={[product]}></Checkout>);
-  expect(() => screen.getByRole("heading",{name:product.title})).not.Throw()
-})
+  expect(() =>
+    screen.getByRole("heading", { name: product.title }),
+  ).not.Throw();
+});
+
+it("renders the selected products when given an array with more than one selected product", () => {
+  const products = [
+    {
+      id: 0,
+      title: "bluh",
+      imageUrl: "bluh",
+      price: 10,
+      rating: { rate: 2, count: 30 },
+      description: "water",
+      selectedAmount: 1,
+    },
+    {
+      id: 2,
+      title: "fire",
+      imageUrl: "flo",
+      price: 10,
+      rating: { rate: 2, count: 30 },
+      description: "fire",
+      selectedAmount: 2,
+    },
+  ];
+  render(<Checkout selectedProducts={products}></Checkout>);
+  expect(() =>
+    screen.getByRole("heading", { name: products[0].title }),
+  ).not.Throw();
+  expect(() =>
+    screen.getByRole("heading", { name: products[1].title }),
+  ).not.Throw();
+});
--- src/components/Products.test.jsx
+++ src/components/Products.test.jsx
@@ -1,9 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { expect, vi, it } from "vitest";
 import Products from "./Products";
-import { useProductsUrl } from "../utils/useUrl";
+import { useProductsUrl, useImageUrl } from "../utils/useUrl";
 vi.mock("../utils/useUrl.js");
-
 it("renders the loading screen when the products haven't been loaded", () => {
   useProductsUrl.mockReturnValue({ loading: true, json: {}, error: false });
   const handleAdding = vi.fn();
@@ -18,4 +17,41 @@ it("renders the error message when error is set true", () => {
   expect(() =>
     screen.getByRole("heading", { name: "Sorry couldn't load products" }),
   ).not.toThrow();
+});
+
+it("renders the the products when the request is successful", () => {
+  const products = [
+    {
+      id: 0,
+      title: "bluh",
+      imageUrl: "bluh",
+      price: 10,
+      rating: { rate: 2, count: 30 },
+      description: "water",
+      selectedAmount: 1,
+    },
+    {
+      id: 2,
+      title: "fire",
+      imageUrl: "flo",
+      price: 10,
+      rating: { rate: 2, count: 30 },
+      description: "fire",
+      selectedAmount: 2,
+    },
+  ];
+  useProductsUrl.mockReturnValue({
+    loading: false,
+    json: products,
+    error: false,
+  });
+  useImageUrl.mockReturnValue({ loading: true, imgSrc: "", error: false });
+  const handleAdding = vi.fn();
+  render(<Products handleAdding={handleAdding}></Products>);
+  expect(() =>
+    screen.getByRole("heading", { name: products[0].title }),
+  ).not.Throw();
+  expect(() =>
+    screen.getByRole("heading", { name: products[1].title }),
+  ).not.Throw();
 });
